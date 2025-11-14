247ed7cbe827fa19b75b3b1f1f8ee57bfb0bf3c7
Kristian Ehlert <kristianehlert@gmail.com>
2025-11-14 16:13:46 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Learn Japanese with YouTube</h1>
+      <p className="mt-4">Starter project is running 🎉</p>
+    </div>
+  );
+}
---
+++ justfile
@@ -0,0 +1,16 @@
+set shell := ["bash", "-cu"]
+
+install:
+    npm install
+
+dev:
+    npm run dev
+
+build:
+    npm run build
+
+start:
+    npm run start
+
+lint:
+    npm run lint
